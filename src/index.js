0153f3e2036fb5ece6398546b519b224bfe2e30b
Nathan Faucett <nathanfaucett@gmail.com>
2015-02-25 18:43:17 -0600

    updates

--- src/index.js
+++ src/index.js
@@ -3,12 +3,24 @@ var isArray = require("is_array"),
     isObject = require("is_object"),
     format = require("format"),
     fastSlice = require("fast_slice"),
-    mixin = require("mixin");
+    has = require("has"),
+    defineProperty = require("define_property");
 
 
-var translationCache = {},
+var translationCache = global.__I18N_TRANSLATIONS__,
     flatMode = false,
     throwMissingError = false;
+
+
+if (!translationCache) {
+    translationCache = {};
+    defineProperty(global, "__I18N_TRANSLATIONS__", {
+        configurable: false,
+        enumerable: false,
+        writable: false,
+        value: translationCache
+    });
+}
 
 
 module.exports = i18n;
@@ -19,7 +31,7 @@ function i18n(locale, key) {
 }
 
 i18n.translate = function(locale, key, args) {
-    var translations = i18n.get(locale);
+    var translations = translationCache[locale] || null;
 
     if (translations === null) {
         throw new Error("i18n(key[, locale[, ...args]]) no translations for " + locale + " locale");
@@ -46,31 +58,27 @@ i18n.throwMissingError = function(value) {
 };
 
 i18n.reset = function() {
-    translationCache = {};
     flatMode = false;
     throwMissingError = false;
 };
 
-i18n.get = function(locale) {
-    return translationCache[locale] || null;
-};
-
-i18n.set = function(locale, object) {
-    if (!isObject(object)) {
-        throw new TypeError("i18n.set(locale, object) object must be an Object");
-    }
-
-    translationCache[locale] = object;
-};
-
 i18n.add = function(locale, object) {
-    var translations = translationCache[locale] || (translationCache[locale] = {});
+    var translations = translationCache[locale] || (translationCache[locale] = {}),
+        key;
 
     if (!isObject(object)) {
         throw new TypeError("i18n.add(locale, object) object must be an Object");
+    } else {
+        for (key in object) {
+            if (has(object, key)) {
+                if (has(translations, key)) {
+                    throw new TypeError("i18n.add(locale, object) cannot override " + locale + " translation with key " + key);
+                } else {
+                    translations[key] = object[key];
+                }
+            }
+        }
     }
-
-    mixin(translations, object);
 };
 
 function missingTranslation(key) {
@@ -105,9 +113,9 @@ function translate(key, translations, args) {
 
     if (value == null || isObject(value)) {
         return missingTranslation(origKey);
+    } else {
+        return args.length !== 0 ? format.args(value, args) : value;
     }
-
-    return args.length !== 0 ? format.args(value, args) : value;
 }
 
 function translateFlat(key, translations, args) {
@@ -115,7 +123,7 @@ function translateFlat(key, translations, args) {
 
     if (value == null || isObject(value)) {
         return missingTranslation(key);
+    } else {
+        return args.length !== 0 ? format.args(value, args) : value;
     }
-
-    return args.length !== 0 ? format.args(value, args) : value;
 }
--- test/test.js
+++ test/test.js
@@ -3,62 +3,45 @@ var assert = require("assert"),
 
 
 describe("i18n", function() {
-    describe("#i18n.get(locale : String)", function() {
-        it("should return translation hash if avalable else returns null", function() {
-            var translations = {
-                key: "value"
-            };
-
-            assert.equal(i18n.get("en"), null);
-
-            i18n.set("en", translations);
-            assert.equal(i18n.get("en"), translations);
-
-            i18n.reset();
-        });
-    });
-
-    describe("#i18n.set(locale : String, object : Object)", function() {
-        it("should set translation hash if not already set", function() {
-            var translations = {
-                key: "value"
-            };
-
-            i18n.set("en", translations);
-            assert.equal(i18n.get("en"), translations);
-            i18n.reset();
-        });
-    });
 
     describe("#i18n.add(locale : String, object : Object)", function() {
         it("should add translation to translation hash", function() {
-            var translations = {
-                key: "value"
-            };
+            delete global.__I18N_TRANSLATIONS__.en;
 
-            i18n.set("en", translations);
             i18n.add("en", {
-                key2: "value2"
-            });
-            assert.deepEqual(i18n.get("en"), {
                 key: "value",
                 key2: "value2"
             });
+            assert.deepEqual(global.__I18N_TRANSLATIONS__.en, {
+                key: "value",
+                key2: "value2"
+            });
+        });
 
-            i18n.reset();
+        it("should throw an error when trying to override translations", function() {
+            delete global.__I18N_TRANSLATIONS__.en;
+
+            i18n.add("en", {
+                key: "value"
+            });
+            try {
+                i18n.add("en", {
+                    key: "value2"
+                });
+            } catch (e) {
+                assert.equal(e.message, "i18n.add(locale, object) cannot override en translation with key key");
+            }
         });
     });
 
     describe("#i18n(locale : String, key : String)", function() {
         it("should return value from locale translation hash", function() {
-            var translations = {
+            delete global.__I18N_TRANSLATIONS__.en;
+
+            i18n.add("en", {
                 key: "value"
-            };
-
-            i18n.set("en", translations);
+            });
             assert.equal(i18n("en", "key"), "value");
-
-            i18n.reset();
         });
     });
 });
