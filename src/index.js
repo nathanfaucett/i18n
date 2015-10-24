c7d24f7cd42fc5fae00d823de91a0921ebd6ce31
nathanfaucett <nathanfaucett@gmail.com>
2015-10-24 09:16:56 -0500

    updates

--- src/index.js
+++ src/index.js
@@ -7,9 +7,7 @@ var isArray = require("is_array"),
     defineProperty = require("define_property");
 
 
-var translationCache = global.__I18N_TRANSLATIONS__,
-    flatMode = false,
-    throwMissingError = false;
+var translationCache = global.__I18N_TRANSLATIONS__;
 
 
 if (!translationCache) {
@@ -23,107 +21,118 @@ if (!translationCache) {
 }
 
 
-module.exports = i18n;
+module.exports = create(false, false);
 
 
-function i18n(locale, key) {
-    return i18n.translate(locale, key, fastSlice(arguments, 2));
-}
+function create(flatMode, throwMissingError) {
 
-i18n.translate = function(locale, key, args) {
-    var translations = translationCache[locale] || null;
+    flatMode = !!flatMode;
+    throwMissingError = !!throwMissingError;
 
-    if (translations === null) {
-        throw new Error("i18n(key[, locale[, ...args]]) no translations for " + locale + " locale");
-    }
-    if (!isString(key)) {
-        throw new TypeError("i18n(key[, locale[, ...args]]) key must be a String");
+
+    function i18n(locale, key) {
+        return i18n.translate(locale, key, fastSlice(arguments, 2));
     }
 
-    args = isArray(args) ? args : [];
+    i18n.create = create;
 
-    if (flatMode === true) {
-        return translateFlat(key, translations, args);
-    } else {
-        return translate(key, translations, args);
-    }
-};
+    i18n.translate = function(locale, key, args) {
+        var translations = translationCache[locale] || null;
 
-i18n.flatMode = function(value) {
-    flatMode = !!value;
-};
+        if (translations === null) {
+            throw new Error("i18n(key[, locale[, ...args]]) no translations for " + locale + " locale");
+        }
+        if (!isString(key)) {
+            throw new TypeError("i18n(key[, locale[, ...args]]) key must be a String");
+        }
 
-i18n.throwMissingError = function(value) {
-    throwMissingError = !!value;
-};
+        args = isArray(args) ? args : [];
 
-i18n.reset = function() {
-    flatMode = false;
-    throwMissingError = false;
-};
+        if (flatMode === true) {
+            return translateFlat(key, translations, args);
+        } else {
+            return translate(key, translations, args);
+        }
+    };
 
-i18n.add = function(locale, object) {
-    var translations = translationCache[locale] || (translationCache[locale] = {}),
-        key;
+    i18n.flatMode = function(value) {
+        flatMode = !!value;
+    };
 
-    if (isObject(object)) {
-        for (key in object) {
-            if (has(object, key)) {
-                if (has(translations, key)) {
-                    throw new TypeError("i18n.add(locale, object) cannot override " + locale + " translation with key " + key);
-                } else {
-                    translations[key] = object[key];
+    i18n.throwMissingError = function(value) {
+        throwMissingError = !!value;
+    };
+
+    i18n.reset = function() {
+        flatMode = false;
+        throwMissingError = false;
+    };
+
+    i18n.add = function(locale, object) {
+        var translations = translationCache[locale] || (translationCache[locale] = {}),
+            key;
+
+        if (isObject(object)) {
+            for (key in object) {
+                if (has(object, key)) {
+                    if (has(translations, key)) {
+                        throw new TypeError("i18n.add(locale, object) cannot override " + locale + " translation with key " + key);
+                    } else {
+                        translations[key] = object[key];
+                    }
                 }
             }
+        } else {
+            throw new TypeError("i18n.add(locale, object) object must be an Object");
         }
-    } else {
-        throw new TypeError("i18n.add(locale, object) object must be an Object");
+    };
+
+    function missingTranslation(key) {
+        if (throwMissingError) {
+            throw new Error("i18n(locale, key) missing translation for key " + key);
+        } else {
+            return "--" + key + "--";
+        }
     }
-};
 
-function missingTranslation(key) {
-    if (throwMissingError) {
-        throw new Error("i18n(locale, key) missing translation for key " + key);
-    } else {
-        return "--" + key + "--";
-    }
-}
-
-function translate(key, translations, args) {
-    var origKey = key,
-        keys = key.split("."),
-        length = keys.length - 1,
-        i = 0,
-        value = translations[keys[i]];
+    function translate(key, translations, args) {
+        var origKey = key,
+            keys = key.split("."),
+            length = keys.length - 1,
+            i = 0,
+            value = translations[keys[i]];
 
 
-    while (i++ < length) {
-        key = keys[i];
+        while (i++ < length) {
+            key = keys[i];
 
-        if (isObject(value)) {
-            value = value[key];
+            if (isObject(value)) {
+                value = value[key];
 
-            if (value == null) {
+                if (value == null) {
+                    return missingTranslation(origKey);
+                }
+            } else {
                 return missingTranslation(origKey);
             }
-        } else {
+        }
+
+        if (value == null || isObject(value)) {
             return missingTranslation(origKey);
+        } else {
+            return args.length !== 0 ? format.args(value, args) : value;
         }
     }
 
-    if (value == null || isObject(value)) {
-        return missingTranslation(origKey);
-    } else {
-        return args.length !== 0 ? format.args(value, args) : value;
-    }
-}
+    function translateFlat(key, translations, args) {
+        var value = translations[key];
 
-function translateFlat(key, translations, args) {
-    var value = translations[key];
-
-    if (value == null || isObject(value)) {
-        return missingTranslation(key);
-    } else {
-        return args.length !== 0 ? format.args(value, args) : value;
+        if (value == null || isObject(value)) {
+            return missingTranslation(key);
+        } else {
+            return args.length !== 0 ? format.args(value, args) : value;
+        }
     }
+
+    return i18n;
 }
